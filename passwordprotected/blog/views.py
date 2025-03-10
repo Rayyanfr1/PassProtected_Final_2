87a9ee7f1665b5022f6061a5899ab89d624dc794
Rayyanfr1 <7a6z2prx@students.codeinstitute.net>
2025-03-10 11:43:37 +0000

    fix: handle invalid post IDs in post_detail

--- passwordprotected/blog/views.py
+++ passwordprotected/blog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, redirect
 from .forms import PostForm  # Make sure to import the PostForm
+from django.shortcuts import get_object_or_404
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -20,3 +21,8 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
