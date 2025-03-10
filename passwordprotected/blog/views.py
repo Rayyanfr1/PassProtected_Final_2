6d66b6d836e6f0ba2d236da70f5dcb545615512b
Rayyanfr1 <7a6z2prx@students.codeinstitute.net>
2025-03-10 11:58:31 +0000

    feat: add confirmation for post_remove

--- passwordprotected/blog/views.py
+++ passwordprotected/blog/views.py
@@ -50,5 +50,7 @@ def post_edit(request, pk):
 
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    post.delete()
-    return redirect('post_list')
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'blog/post_confirm_delete.html', {'post': post})
