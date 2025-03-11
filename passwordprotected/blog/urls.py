982da2f70229b9c331e7c71ec5d2443b4cc28c68
Rayyanfr1 <7a6z2prx@students.codeinstitute.net>
2025-03-11 11:44:14 +0000

    feat: add URL pattern for password_generator

--- passwordprotected/blog/urls.py
+++ passwordprotected/blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
     path('password_checker/', views.password_checker, name='password_checker'),
+    path('password_generator/', views.password_generator, name='password_generator'),
 ]
